b47862cb56e982dee49204ea96c7ed23821e62a3
Keeth <shadowtheplayeryt@gmail.com>
2025-07-26 23:43:52 +1000

    Framework
    yurrrr

--- src/main/java/me/BMC/pKHackathon/MoveListener.java
+++ src/main/java/me/BMC/pKHackathon/MoveListener.java
@@ -1,12 +1,30 @@
 package me.BMC.pKHackathon;
 
+import com.projectkorra.projectkorra.BendingPlayer;
+import com.projectkorra.projectkorra.PKListener;
+import com.projectkorra.projectkorra.ability.CoreAbility;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerToggleSneakEvent;
+import me.BMC.pKHackathon.PKHackathon;
 
 public class MoveListener implements Listener {
+    public PKHackathon ability = PKHackathon.getAbility();
     @EventHandler
     public void onPlayerSneak(PlayerToggleSneakEvent event) {
+        if (!event.isSneaking()) return;
+        final Player player = event.getPlayer();
+        final BendingPlayer bPlayer = BendingPlayer.getBendingPlayer(player);
+        if (!bPlayer.canBend())
+        if (bPlayer == null) return;
+        String bound = bPlayer.getBoundAbilityName();
+        if (bound.equalsIgnoreCase("PKHackathon")) {
+            final PKHackathon move = CoreAbility.getAbility(player, PKHackathon.class);
 
+            if (player.getLocation().subtract(0, PKHackathon.mobilityHeight, 0).getBlock().getType() == Material.SAND && move != null) {
+                new PKHackathon(player);
+            }
+        }
     }
 }
--- src/main/java/me/BMC/pKHackathon/PKHackathon.java
+++ src/main/java/me/BMC/pKHackathon/PKHackathon.java
@@ -1,26 +1,68 @@
 package me.BMC.pKHackathon;
 
+import com.projectkorra.projectkorra.ProjectKorra;
 import com.projectkorra.projectkorra.ability.AddonAbility;
 import com.projectkorra.projectkorra.ability.AirAbility;
+import com.projectkorra.projectkorra.configuration.ConfigManager;
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
+import org.bukkit.entity.Boat;
 import org.bukkit.entity.Player;
+import org.bukkit.permissions.Permission;
 
 public class PKHackathon extends AirAbility implements AddonAbility {
+
+    public double mobilityHeight = ConfigManager.getConfig().getDouble("ExtraAbilities.Sand.PKHackathon.RideHeight", 2); // CHANGEME
+    public final double mobilitySpeed = ConfigManager.getConfig().getDouble("ExtraAbilities.Sand.PKHackathon.SeatingSpeed"); //  CHANGEME
+    public final double tornadoLifetime = ConfigManager.getConfig().getDouble("ExtraAbilities.Sand.PKHackathon.Lifetime");
+
+    private Permission perm; // DONT CHANGEME
+
     public PKHackathon(Player player) {
         super(player);
 
 
     }
+
     public void load() {
+        ProjectKorra.log.info(getName() + "has loaded!" + "\n" + getDescription());
+        Bukkit.getPluginManager().registerEvents(new MoveListener(), ProjectKorra.plugin);
+
+        perm = new Permission("bending.ability.PKHackathon");
+
+        ConfigManager.getConfig().addDefault("ExtraAbilities.Sand.PKHackathon.RideHeight", 2);
+        ConfigManager.getConfig().addDefault("ExtraAbilities.Sand.PKHackathon.SeatingSpeed", 0);
+        ConfigManager.getConfig().addDefault("ExtraAbilities.Sand.PKHackathon.Cooldown", 5000);
+        ConfigManager.getConfig().addDefault("ExtraAbilities.Sand.PKHackathon.Lifetime", 5000);
 
     }
+
     public void progress() {
-
+        // if there are reasons to cancel move canel move
+        if (System.currentTimeMillis() - getStartTime() >= tornadoLifetime) {
+            this.remove();
+            return;
+        }
+        if (player.getVehicle() != null) {
+            if (player.getVehicle() instanceof Boat boat) { // Maybe swap for getType() ==
+            // new method tickBoatMovement
+            // new method drawParticles
+                tickBoatMovement(boat);
+                drawParticles();
+            return;
+        }
+        }
     }
+
+    public void stop() {
+      this.remove();
+      Bukkit.getServer().getPluginManager().removePermission(perm);
+    }
+
 
     @Override
     public boolean isSneakAbility() {
-        return false;
+        return true;
     }
 
     @Override
@@ -35,25 +77,40 @@ public class PKHackathon extends AirAbility implements AddonAbility {
 
     @Override
     public String getName() {
-        return "";
+        return "PKHackathon";
     }
+
+    @Override
+    public String getInstructions(){
+        return "press x to pay respect"; // CHANGEME
+    }
+
+    @Override
+    public String getDescription(){
+        return "Sand Tornado go brrrr"; // CHANGEME
+    }
+
 
     @Override
     public Location getLocation() {
         return null;
     }
 
-    public void stop() {
-
-    }
-
     @Override
     public String getAuthor() {
-        return "RyanDusty, ShadowThePlayer, SnowyOwl217";
+        return "RyanDusty, ShadowTP, SnowyOwl217";
     }
 
     @Override
     public String getVersion() {
         return "1.0";
+    }
+
+    public void drawParticles() {
+
+    }
+
+    public void tickBoatMovement(Boat boat) {
+         //
     }
 }
